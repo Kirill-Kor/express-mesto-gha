f4409cdaa87daf3dd928a74bc891b8ed2f9089a5
Kirill Kor <grav.kor@yandex.ru>
2023-01-16 01:10:09 +0500

    fixes/eslint

--- controllers/users.js
+++ controllers/users.js
@@ -1,43 +1,37 @@
 const User = require('../models/user');
 
-const {NOT_FOUND_STATUS_CODE, NOT_FOUND_USER_MESSAGE} = require('../utils/errors')
-
+const { NOT_FOUND_STATUS_CODE, NOT_FOUND_USER_MESSAGE } = require('../utils/errors');
 
 const getUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
     res.send(users);
-
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
-
-}
+};
 
 const getUserById = async (req, res, next) => {
   try {
-    const user = await User.findById(req.params.userId)
+    const user = await User.findById(req.params.userId);
     if (user === null) {
       res.status(NOT_FOUND_STATUS_CODE).send(NOT_FOUND_USER_MESSAGE);
     }
     res.send({ data: user });
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 const createUser = async (req, res, next) => {
   const { name, about, avatar } = req.body;
   try {
-    const user = await User.create({ name, about, avatar })
+    const user = await User.create({ name, about, avatar });
     res.send(user);
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 const patchUserInfo = async (req, res, next) => {
   const { name, about } = req.body;
@@ -45,39 +39,38 @@ const patchUserInfo = async (req, res, next) => {
   try {
     const user = await User.findByIdAndUpdate(
       req.user._id,
-      { name: name, about: about },
+      { name, about },
       {
         new: true,
-        runValidators: true
-      })
+        runValidators: true,
+      },
+    );
     if (user === null) {
       res.status(NOT_FOUND_STATUS_CODE).send(NOT_FOUND_USER_MESSAGE);
     }
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 const patchUserAvatar = async (req, res, next) => {
   const { avatar } = req.body;
   try {
     const user = User.findByIdAndUpdate(
       req.user._id,
-      { avatar: avatar },
+      { avatar },
       {
         new: true,
-        runValidators: true
-      })
+        runValidators: true,
+      },
+    );
     if (user === null) {
       res.status(NOT_FOUND_STATUS_CODE).send(NOT_FOUND_USER_MESSAGE);
     }
-  }
-  catch(error) {
+  } catch (error) {
     next(error);
   }
-
-}
+};
 
 module.exports = {
   getUsers,
@@ -85,4 +78,4 @@ module.exports = {
   createUser,
   patchUserInfo,
   patchUserAvatar,
-}
+};
