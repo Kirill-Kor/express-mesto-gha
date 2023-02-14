80ed898eef4d259202bc123063235fe2fafd2728
Kirill Kor <grav.kor@yandex.ru>
2023-02-14 16:36:36 +0500

    cors fixes 10

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://kirkors.mesto.nomoredomains.work');
 
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', origin);
