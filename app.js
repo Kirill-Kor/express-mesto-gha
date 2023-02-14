78f461ed3892df36ab83dd07186d22c4f332910b
Kirill Kor <grav.kor@yandex.ru>
2023-02-14 15:46:34 +0500

    cors fixes 4

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  res.send(req.headers);
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
