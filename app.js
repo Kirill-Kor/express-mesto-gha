1b755d952f5b2c47eae4d72b1439981616632a36
Kirill Kor <grav.kor@yandex.ru>
2023-02-14 15:55:23 +0500

    cors fixes 7

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.uncludes(origin)) {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
