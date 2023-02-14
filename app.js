a9eddb30d4c70183adeb63b645f625cb6814956e
Kirill Kor <grav.kor@yandex.ru>
2023-02-14 15:40:37 +0500

    cors fixes 3

--- app.js
+++ app.js
@@ -24,13 +24,14 @@ const allowedCors = [
 ];
 
 app.use((req, res, next) => {
-  const { host } = req.headers;
+  const { origin } = req.headers;
   const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.uncludes(host)) {
-    res.header('Access-Control-Allow-Origin', host);
+  if (allowedCors.uncludes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
